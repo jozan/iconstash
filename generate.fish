7042398e0551b647a6eb8aad4cbb01d84fbb077e
Johan Ruokangas <johan@latehours.net>
2023-07-21 15:06:14 +0300

    tussi

--- generate.fish
+++ generate.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
+# heroicons
 set dir_path "./output/heroicons-solid-24"
 
 set output_file "./heroicons.ts"
@@ -20,4 +21,22 @@ for file in $dir_path/*.svg
 end
 
 echo "] as const" >> $output_file
+
+# ionicons
+set dir_path "./output/ionicons/solid"
+set output_file "./ionicons.ts"
+set filter_list "./filter_ionicons"
+touch $filter_list
+
+echo "export const IonIcons = [" > $output_file
+
+for file in $dir_path/*.svg
+    set filename (basename -- "$file" .svg)
+
+    if rg -Fxq $filename $filter_list
+        continue
+    end
+
+    echo "{ id: \"ionicon-$filename\", getIcon: () => require(\"./ionicons/solid/$filename.svg\") }, " >> $output_file
+end
 
