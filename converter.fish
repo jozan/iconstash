2226e064d87dd3604fae53336f1732c79f6f06c5
Johan Ruokangas <johan@latehours.net>
2023-07-21 14:58:49 +0300

    tussi

--- converter.fish
+++ converter.fish
@@ -8,4 +8,26 @@ find ./node_modules/heroicons/24/solid -type f -name "*.svg" | while read -l fil
     cp $file ./output/heroicons-solid-24/$newname.svg
 end
 
+# ionicons
+set ionicons_output_dir "./output/ionicons"
+mkdir -p $ionicons_output_dir
+
+for f in ./node_modules/ionicons/dist/svg/*.svg
+  switch $f
+    case '*-outline.svg'
+      set dir "outline"
+    case '*-sharp.svg'
+      set dir "sharp"
+    case '*'
+      set dir "solid"
+  end
+
+  mkdir -p $ionicons_output_dir/$dir
+
+  set name (basename $f .svg)
+  set newname (echo $name | awk -F"-" '{for(i=1;i<=NF;i++){printf toupper(substr($i,1,1)) substr($i,2)""}} END{print "Icon"}')
+
+  cp $f $ionicons_output_dir/$dir/$newname.svg
+end
+
 bun run svgo
